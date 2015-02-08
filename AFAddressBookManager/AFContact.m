337ea9417f478e62c21d0e4a3cd8e68d9ed091a0
Anders Fogh Eriksen <anders@afogh.dk>
2015-02-08 12:28:27 +0100

    Update year!

--- AFAddressBookManager/AFContact.m
+++ AFAddressBookManager/AFContact.m
@@ -2,7 +2,7 @@
 //  AFContact.m
 //
 //  Created by Anders Fogh Eriksen on 09/04/13.
-//  Copyright (c) 2013 Anders Eriksen. All rights reserved.
+//  Copyright (c) 2013-2015 Anders Eriksen. All rights reserved.
 //
 
 #import "AFContact.h"
