fcec41acfb270c14b6fb6bf11e2473fd72617a79
Anders Fogh Eriksen <anders@afogh.dk>
2015-02-17 19:53:10 +0100

    Clean up project

--- Demo/AppDelegate.m
+++ Demo/AppDelegate.m
@@ -10,9 +10,8 @@
 
 @implementation AppDelegate
 
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
     return YES;
 }
 
