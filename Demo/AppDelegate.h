27e628d49e4cc981507d52bf8594609e0451b274
Anders Fogh Eriksen <anders@afogh.dk>
2015-02-17 19:42:54 +0100

    Add project

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFAddressBookManager
+//
+//  Created by Anders Eriksen on 17/02/15.
+//  Copyright (c) 2015 Anders Fogh Eriksen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
