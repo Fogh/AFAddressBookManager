0bacd3dc2c2953048367d22f377545e80ed21e3e
Anders Fogh Eriksen <anders@afogh.dk>
2015-02-17 19:42:54 +0100

    Add project

---
+++ Demo/AppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.m
+//  AFAddressBookManager
+//
+//  Created by Anders Eriksen on 17/02/15.
+//  Copyright (c) 2015 Anders Fogh Eriksen. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
---
+++ Demo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  AFAddressBookManager
+//
+//  Created by Anders Eriksen on 17/02/15.
+//  Copyright (c) 2015 Anders Fogh Eriksen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
